6f19785b9bd393b9a69e4f6e964c76c4f3f6387d
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-05-18 16:01:26 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,16 @@
+# %%
+# Main libries for Ml  
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# %%
+print("TensorFlow v" + tf.__version__)
+
+# %%
+#file 
+
+
